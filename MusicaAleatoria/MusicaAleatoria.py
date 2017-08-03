329eb91fb9890929f6809170f70a3e39ddd77550
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2017-08-04 01:00:12 +0200

    Casi hecho

--- MusicaAleatoria/MusicaAleatoria.py
+++ MusicaAleatoria/MusicaAleatoria.py
@@ -1,9 +1,14 @@
 from os import *
 from glob import glob
+extension=".py"
 def listar(ruta):
-        return [arch.name for arch in scandir(ruta) if arch.is_file()]  
+        return [arch.name for arch in scandir(ruta) if arch.is_file() and ispython(arch.name )]
 
-
+def ispython(nombre):
+    if(nombre[len(nombre)-len(extension):]==extension):
+         return True
+    else:
+       return False
 
 def renombrar(lista):
     for cancion in lista:
