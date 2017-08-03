32b611fa5565b25f156185a7a5da98a9b93f3f50
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2017-08-04 00:46:49 +0200

    Empezando

--- MusicaAleatoria/MusicaAleatoria.py
+++ MusicaAleatoria/MusicaAleatoria.py
@@ -1,1 +1,18 @@
+from os import *
+from glob import glob
+def listar(ruta):
+        return [arch.name for arch in scandir(ruta) if arch.is_file()]  
 
+
+
+def renombrar(lista):
+    for cancion in lista:
+        print(cancion)
+def main():
+    print("Musica en :"+ getcwd())
+    lista=listar(getcwd())
+    renombrar(lista)
+
+
+if __name__=="__main__":
+    main()
