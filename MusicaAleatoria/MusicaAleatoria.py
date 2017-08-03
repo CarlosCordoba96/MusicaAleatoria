392e5039f08b0795aa26ba9472101b6a5972b500
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2017-08-04 01:16:21 +0200

    Falta el renombramiento aleatorio

--- MusicaAleatoria/MusicaAleatoria.py
+++ MusicaAleatoria/MusicaAleatoria.py
@@ -1,21 +1,29 @@
 from os import *
 from glob import glob
-extension=".py"
-def listar(ruta):
-        return [arch.name for arch in scandir(ruta) if arch.is_file() and ispython(arch.name )]
 
-def ispython(nombre):
+
+def isExtension(nombre,extension):
+
     if(nombre[len(nombre)-len(extension):]==extension):
          return True
     else:
        return False
 
+def listar(ruta,extension):
+        return [arch.name for arch in scandir(ruta) if arch.is_file() and isExtension(arch.name,extension)]
+
+
 def renombrar(lista):
     for cancion in lista:
         print(cancion)
+
 def main():
-    print("Musica en :"+ getcwd())
-    lista=listar(getcwd())
+    print("Introduce la ruta:")
+    RUTA=input()
+   # RUTA="E:\\Users\Carlos\Desktop\prueba"
+    print("Que tipo de formato quieres cambiar: ")
+    extension=input()
+    lista=listar(RUTA,extension)
     renombrar(lista)
 
 
